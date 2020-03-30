80e9a6971b9a2d7f93f4715c29b6966abc13de2d
Matthew Crockett <mattcroc@umich.edu>
2020-03-29 20:25:51 -0400

    edit covidmain and covidseirode

--- covidmain.m
+++ covidmain.m
@@ -7,5 +7,51 @@
     local regions in Michigan and how different regions might have a 
     different model as a good fit. With the existing data in hand, 
     we would hope to generate a good prediction into the future of the 
-    outbreak.
+    outbreak. 
+
+    We're gonna use the model we used for the ebola outbreak to help get
+    started with the model.
+
+        S is the number of susceptible people.
+        E is the number of 
+        I is the number of infected people.
+        R is the number of recovered people.
+        y is the number of case counts for infection.
+
 %}
+dataCases = load('covid_data.dat'); %we still need to get the data for this
+
+%Plot the data.
+figure
+hold on
+plot(dataA(:,1),dataA(:,2),'.');
+xlabel('Time (days)');
+ylabel('Total numbers of case counts');
+title('Number of case counts as a function of time');
+
+B = 1; %Beta
+A = 1; %Alpha
+r = 1; %Gamma
+N = 1; %This is the total number of people 
+
+%Define the initial conditions.
+I0 = dataCases(1,2)./N;
+E0 = 2.*I0;
+S0 = 1 - I0 - E0;
+R0 = 0;
+y0 = N.*A.*E0;
+
+%Define the vectors for the parameters and initial conditions.
+params = [B,A,r,N];
+x0 = [S0,E0,I0,R0,y0];
+tspan = dataCases(:,1);
+
+%Call the function seirode to evaluate the values and then plot the
+%ydot solutions vs time.
+options = odeset('AbsTol',1e-8,'RelTol',1e-8);
+fun = @(t,x) covidseirode(t,x,params);
+[t,xsol] = ode45(fun,tspan,x0,options);
+plot(tspan,xsol(:,5),'*');
+
+
+
--- covidseirode.m
+++ covidseirode.m
@@ -1,3 +1,33 @@
 %{
     This function will evaluate the SEIR model.
+    
+    This is the model we used for the ebola outbreak problem from lab 3:
+
+    Sdot = -BSI                   Change in susceptible population
+    Edot = BSI - AE               Change in susceptible population
+    Idot = AE - rI                Change in susceptible population
+    Rdot = rI                     Change in susceptible population
+    ydot = NAE                    Change in susceptible population
+    
 %}
+function dydt = covidseirode(t,x,params)
+
+    B = params(1);
+    A = params(2);
+    r = params(3);
+    N = params(4);
+    
+    S = x(1);
+    E = x(2);
+    I = x(3);
+    R = x(4);
+    y = x(5);
+
+    Sdot = -B.*S.*I;
+    Edot = B.*S.*I - A.*E;
+    Idot = A.*E - r.*I;
+    Rdot = r.*I;
+    ydot = N.*A.*E;
+    dxdt = [Idot;Edot;Sdot;Rdot;ydot];
+    
+end
